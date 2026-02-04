87e1d89806170b3ee3b3fb02a281f973b877cf87
sorozco <soorozco2027@gmail.com>
2026-02-04 14:44:55 -0800

    Initial Commit

---
+++ DungeonDice/DungeonDiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  DungeonDiceApp.swift
+//  DungeonDice
+//
+//  Created by OROZCO, SOFIA on 2/4/26.
+//
+
+import SwiftUI
+
+@main
+struct DungeonDiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DungeonDiceTests/DungeonDiceTests.swift
@@ -0,0 +1,17 @@
+//
+//  DungeonDiceTests.swift
+//  DungeonDiceTests
+//
+//  Created by OROZCO, SOFIA on 2/4/26.
+//
+
+import Testing
+@testable import DungeonDice
+
+struct DungeonDiceTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
